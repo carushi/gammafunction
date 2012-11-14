7d903ba20c4a5a4c3d864db799317a03711e5d43
carushi <trumpet-lambda@hotmail.co.jp>
2012-11-14 19:15:21 +0900

    Update gamma.c
    revised indents...

--- gamma.c
+++ gamma.c
@@ -10,13 +10,13 @@ int main(int argc, char **argv)
 {
   int N = 12829, n = 4276, m = 11600, k = 4268, K = k+1, i;
   double firstprobability = probability(N, n, m, k), sum = firstprobability;
-  printf("1st%lf ", firstprobability);
-  printf("2nd%lf ", firstprobability*(double)(m-K+1)/(double)(K)*(double)(n-K+1)/(double)(N-m-n+K));
+  printf("1st %lf ", firstprobability);
+  printf("2nd %lf ", firstprobability*(double)(m-K+1)/(double)(K)*(double)(n-K+1)/(double)(N-m-n+K));
   printf("2nd %lf \n", probability(N, n, m, k+1));
   for (i = 1; i +k <= n && i+k <= m; i++) 
-    {
+  {
      sum += probability(N, n, m, k+i);
-    }
+  }
   printf("sum %.20lf\n", sum);
   return 0;
 
